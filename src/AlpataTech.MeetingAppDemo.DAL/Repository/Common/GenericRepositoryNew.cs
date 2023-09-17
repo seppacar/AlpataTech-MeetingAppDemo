1ae6189d57a179fd8cbb906ffa1fac06f0aa1a91
seppacar <sepp.acar@gmail.com>
2023-09-17 18:12:20 +0300

    migrating to async methods: created asynchronous generic repository

---
+++ src/AlpataTech.MeetingAppDemo.DAL/Repository/Common/GenericRepositoryNew.cs
@@ -0,0 +1,66 @@
+﻿using AlpataTech.MeetingAppDemo.DAL.Context;
+using AlpataTech.MeetingAppDemo.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AlpataTech.MeetingAppDemo.DAL.Repository.Common
+{
+    public abstract class GenericRepositoryNew<T> : IGenericRepositoryNew<T> where T : BaseEntity
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepositoryNew(ApplicationDbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = dbSet;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/AlpataTech.MeetingAppDemo.DAL/Repository/Common/IGenericRepositoryNew.cs
@@ -0,0 +1,18 @@
+﻿using AlpataTech.MeetingAppDemo.Entities.Common;
+using System.Linq.Expressions;
+
+namespace AlpataTech.MeetingAppDemo.DAL.Repository.Common
+{
+    public interface IGenericRepositoryNew<T> where T : BaseEntity
+    {
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        void Update(T entity);
+        void Remove(T entity);
+        Task SaveChangesAsync(); // Async version
+        void SaveChanges(); // Synchronous version
+    }
+}
