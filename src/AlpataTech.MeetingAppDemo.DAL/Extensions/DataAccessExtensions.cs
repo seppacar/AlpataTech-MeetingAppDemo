f371aa2db9fc076bce35af3db0c2b7c7a7e09a1f
seppacar <sepp.acar@gmail.com>
2023-09-18 01:22:14 +0300

    Added DAL Service Extensions

---
+++ src/AlpataTech.MeetingAppDemo.DAL/Extensions/DataAccessExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AlpataTech.MeetingAppDemo.DAL.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlpataTech.MeetingAppDemo.DAL.Extensions
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection SetupDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
