2963c7e7fbb826d0513a446b9e12bb0c71287b61
seppacar <sepp.acar@gmail.com>
2023-09-23 09:51:59 +0300

    Model creation uniqueness constraints

--- src/AlpataTech.MeetingAppDemo.DAL/Context/ApplicationDbContext.cs
+++ src/AlpataTech.MeetingAppDemo.DAL/Context/ApplicationDbContext.cs
@@ -19,6 +19,15 @@ namespace AlpataTech.MeetingAppDemo.DAL.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            // Set alternate key "Email" or maybe set Unique index?
+            modelBuilder.Entity<User>()
+                .HasAlternateKey(u => u.Email);
+
+            // Configure a composite unique index for UserId and MeetingId
+            modelBuilder.Entity<MeetingParticipant>()
+                .HasIndex(e => new { e.UserId, e.MeetingId })
+                .IsUnique();
+
             // AutoInclude navigation properties
             modelBuilder.Entity<Meeting>()
                 .Navigation(m => m.Documents)
@@ -28,7 +37,7 @@ namespace AlpataTech.MeetingAppDemo.DAL.Context
                 .AutoInclude();
             modelBuilder.Entity<Meeting>()
                 .Navigation(m => m.Organizer)
-                .AutoInclude();
+                .AutoInclude(); 
             modelBuilder.Entity<User>()
                 .Navigation(u => u.Roles)
                 .AutoInclude();
@@ -39,137 +48,11 @@ namespace AlpataTech.MeetingAppDemo.DAL.Context
                 .Navigation(mp => mp.User)
                 .AutoInclude();
 
-            // Seed roles (assuming you have a Roles DbSet)
+            // Seed roles
             modelBuilder.Entity<Role>().HasData(
                 new Role { Id = 1, Name = "Admin" },
                 new Role { Id = 2, Name = "User" }
             // Add other roles as needed
-            );
-
-            // Seed Users
-            modelBuilder.Entity<User>().HasData(
-            new User[]
-            {
-                    new User()
-                    {
-                        Id = 1,
-                        FirstName = "User1 FN",
-                        LastName = "User1 LN",
-                        Email = "user1@test.com",
-                        PhoneNumber = "1234567890",
-                        PasswordHash = "1234567890",
-                        ProfileImage = "test.png"
-                    },
-                    new User()
-                    {
-                        Id = 2,
-                        FirstName = "User2 FN",
-                        LastName = "User2 LN",
-                        Email = "user2@test.com",
-                        PhoneNumber = "1234567890",
-                        PasswordHash = "1234567890",
-                        ProfileImage = "test.png"
-                    },
-                    new User()
-                    {
-                        Id = 3,
-                        FirstName = "User3 FN",
-                        LastName = "User3 LN",
-                        Email = "user3@test.com",
-                        PhoneNumber = "1234567890",
-                        PasswordHash = "1234567890",
-                        ProfileImage = "test.png"
-                    },
-                    new User()
-                    {
-                        Id = 4,
-                        FirstName = "User4 FN",
-                        LastName = "User4 LN",
-                        Email = "user3@test.com",
-                        PhoneNumber = "1234567890",
-                        PasswordHash = "1234567890",
-                        ProfileImage = "test.png"
-                    }
-            }
-            );
-
-            // Seed UserRole (join table) to establish relationships
-            modelBuilder.Entity<UserRole>().HasData(
-                new UserRole { Id = 1, UserId = 1, RoleId = 1 },
-                new UserRole { Id = 2, UserId = 2, RoleId = 1 },
-                new UserRole { Id = 3, UserId = 3, RoleId = 2 },
-                new UserRole { Id = 4, UserId = 4, RoleId = 2 },
-                new UserRole { Id = 5, UserId = 1, RoleId = 2 }
-            );
-
-            // Seed Meetings
-            modelBuilder.Entity<Meeting>().HasData(
-                new Meeting
-                {
-                    Id = 1,
-                    Title = "Meeting 1",
-                    Description = "Description for Meeting 1",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now.AddHours(2),
-                    OrganizerId = 1, // Assign an organizer (User with Id 1)
-                },
-                new Meeting
-                {
-                    Id = 2,
-                    Title = "Meeting 2",
-                    Description = "Description for Meeting 2",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now.AddHours(2),
-                    OrganizerId = 2, // Assign an organizer (User with Id 2)
-                }
-            );
-
-            // Seed Meeting Documents
-            modelBuilder.Entity<MeetingDocument>().HasData(
-                new MeetingDocument
-                {
-                    Id = 1,
-                    MeetingId = 1, // Associated with Meeting 1
-                    DocumentTitle = "Document 1",
-                    DocumentType = "NONE",
-                    DocumentPath = "path/to/document1.pdf",
-                },
-                new MeetingDocument
-                {
-                    Id = 2,
-                    MeetingId = 2, // Associated with Meeting 2
-                    DocumentTitle = "Document 2",
-                    DocumentType = "NONE",
-                    DocumentPath = "path/to/document2.pdf",
-                }
-            );
-
-            // Seed Meeting Participants (Link Users to Meetings)
-            modelBuilder.Entity<MeetingParticipant>().HasData(
-                new MeetingParticipant
-                {
-                    Id = 1,
-                    MeetingId = 1, // Meeting 1
-                    UserId = 1,     // User 1
-                },
-                new MeetingParticipant
-                {
-                    Id = 2,
-                    MeetingId = 2, // Meeting 2
-                    UserId = 2,     // User 2
-                },
-                new MeetingParticipant
-                {
-                    Id = 3,
-                    MeetingId = 2, // Meeting 2
-                    UserId = 3,     // User 2
-                },
-                new MeetingParticipant
-                {
-                    Id = 4,
-                    MeetingId = 2, // Meeting 2
-                    UserId = 4,     // User 2
-                }
             );
         }
         public DbSet<User> Users { get; set; }
