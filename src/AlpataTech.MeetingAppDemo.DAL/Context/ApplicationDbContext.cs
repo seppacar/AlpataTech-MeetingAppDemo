8448d5d8234c54b04095ecc6d0ed06c7e05c5f02
seppacar <sepp.acar@gmail.com>
2023-09-24 16:12:42 +0300

    remove unnecessary lines

--- src/AlpataTech.MeetingAppDemo.DAL/Context/ApplicationDbContext.cs
+++ src/AlpataTech.MeetingAppDemo.DAL/Context/ApplicationDbContext.cs
@@ -5,20 +5,12 @@ namespace AlpataTech.MeetingAppDemo.DAL.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        //public ApplicationDbContext() { }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        // TODO: Cannot generate migrations rn, remove this later
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // Replace MSSQL Server String TODO: use settings file
-            //optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=DEV_AlpataTechMeetingApp;trusted_connection=true;trustservercertificate=true;");
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
             // Set alternate key "Email" or maybe set Unique index?
             modelBuilder.Entity<User>()
                 .HasAlternateKey(u => u.Email);
