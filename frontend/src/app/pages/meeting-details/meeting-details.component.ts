c546d72bc9361f1812fe73de14899590225a4a2d
seppacar <sepp.acar@gmail.com>
2023-12-18 18:42:51 +0300

    fetch meeting

--- frontend/src/app/pages/meeting-details/meeting-details.component.ts
+++ frontend/src/app/pages/meeting-details/meeting-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Meeting } from '../../core/models/meeting/meeting.model';
+import { MeetingService } from '../../core/services/meeting/meeting.service';
 
 @Component({
   selector: 'app-meeting-details',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './meeting-details.component.scss'
 })
 export class MeetingDetailsComponent {
+  meeting: Meeting | null = null
+
+  constructor(private route: ActivatedRoute, private meetingService: MeetingService) {}
+
+  ngOnInit(){
+    this.getMeeting(this.route.snapshot.params['id'])
+  }
+
+  getMeeting(id: number){
+    this.meetingService.get(id)
+    .subscribe(
+      {
+        next: (meeting) => {console.log(meeting)},
+        error: (error) => {console.error(error)}
+      }
+    )
+  }
 
 }
